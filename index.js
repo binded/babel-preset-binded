5cf57ecf55b43ff1af5004e5c29adda2c6b7f549
Olivier Lalonde <olalonde@gmail.com>
2016-11-28 13:53:26 -0800

    update to new object rest spread

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 module.exports = {
   presets: ['eslatest-node6'],
   plugins: [
-    'transform-es2015-destructuring',
-    'transform-es2015-parameters',
     ['transform-object-rest-spread', { useBuiltIns: true }]
   ],
   env: {
